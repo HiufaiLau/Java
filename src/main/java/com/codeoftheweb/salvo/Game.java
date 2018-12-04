a12ec0f6e5b2d0bdb271fea9be4ff5783e0a19fb
HiufaiLau <hiuailau.41@gmail.com>
2018-12-04 11:25:58 +0100

    started  game class

--- src/main/java/com/codeoftheweb/salvo/Game.java
+++ src/main/java/com/codeoftheweb/salvo/Game.java
@@ -1,4 +1,25 @@
 package com.codeoftheweb.salvo;
 
+import org.hibernate.annotations.GenericGenerator;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private Long id;
+
+    public Game() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
