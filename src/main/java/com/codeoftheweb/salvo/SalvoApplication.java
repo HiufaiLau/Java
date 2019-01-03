8e1820efba016f19a319afa88f61460b21d7ec6a
Hiu <43778267+HiufaiLau@users.noreply.github.com>
2019-01-03 15:36:54 +0100

    added Authentication and Authorization

--- src/main/java/com/codeoftheweb/salvo/SalvoApplication.java
+++ src/main/java/com/codeoftheweb/salvo/SalvoApplication.java
@@ -4,21 +4,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.WebAttributes;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
 
 @SpringBootApplication
@@ -244,9 +248,9 @@ class WebSecurityConfiguration extends GlobalAuthenticationConfigurerAdapter {
     @Override
     public void init(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(inputName-> {
-            Person person = personRepository.findByEmail(inputName);
-            if (person != null) {
-                return new SecurityProperties.User(person.getEmail(), person.getPassword(),
+            Player player = playerRepository.findByEmail(inputName);
+            if (player != null) {
+                return new User(player.getEmail(), player.getPassword(),
                         AuthorityUtils.createAuthorityList("USER"));
             } else {
                 throw new UsernameNotFoundException("Unknown user: " + inputName);
@@ -260,26 +264,46 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/web/allGames.html").permitAll()
-                .antMatchers("/web/styles/allGames.css").permitAll()
-                .antMatchers("/web/scripts/allGames.js").permitAll()
+//                .antMatchers("/admin/**").hasAuthority("ADMIN")
+                .antMatchers("/**").permitAll()
+//                .antMatchers("/web/allGames.html").permitAll()
+//                .antMatchers("/web/game.html").permitAll()
+//                .antMatchers("/web/styles/allGames.css").permitAll()
+//                .antMatchers("/web/scripts/allGames.js").permitAll()
 //                .antMatchers("/web/scripts/login.js").permitAll()
-                .antMatchers("/api/games").permitAll()
-                .antMatchers("/api/leaderBoard").permitAll()
-                .antMatchers("/api/players").permitAll()
-                .formLogin();
+//                .antMatchers("/api/games").permitAll()
+//                .antMatchers("/api/leaderBoard").permitAll()
+//                .antMatchers("/api/players").permitAll()
+                .and()
+            .formLogin()
+                .usernameParameter("name")
+                .passwordParameter("password")
+                .loginPage("/app/login");
+
+        http.logout().logoutUrl("/app/logout");
+
+        // turn off checking for CSRF tokens
+        http.csrf().disable();
+
+        // if user is not authenticated, just send an authentication failure response
+        http.exceptionHandling().authenticationEntryPoint((req, res, exc) -> res.sendError(HttpServletResponse.SC_UNAUTHORIZED));
+
+        // if login is successful, just clear the flags asking for authentication
+        http.formLogin().successHandler((req, res, auth) -> clearAuthenticationAttributes(req));
+
+        // if login fails, just send an authentication failure response
+        http.formLogin().failureHandler((req, res, exc) -> res.sendError(HttpServletResponse.SC_UNAUTHORIZED));
+
+        // if logout is successful, just send a success response
+        http.logout().logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler());
     }
 
-    @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER")
-                        .build();
-
-        return new InMemoryUserDetailsManager(user);
+    private void clearAuthenticationAttributes(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+        }
     }
+
+
 }
